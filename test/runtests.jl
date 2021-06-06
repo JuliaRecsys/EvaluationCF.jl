8c7e49f22d10c1b3ec3c19d84a1eb32e7bd18f48
Filipe Braida <filipebraida@gmail.com>
2021-06-06 10:19:22 -0300

    adding holdout and kfold tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,47 @@
 using Persa
 using Test
 using DataFrames
+using EvaluationCF
 
-function createdummydatasetone()
+function createdummydataset()
     df = DataFrame()
-    df[!, :user] = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7]
-    df[!, :item] = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 4, 5, 6, 2, 4, 5]
-    df[!, :rating] = [2.5, 3.5, 3.0, 3.5, 2.5, 3.0, 3, 3.5, 1.5, 5, 3, 3.5, 2.5, 3.0, 3.5, 4.0, 3.5, 3.0, 4.0, 2.5, 4.5, 3.0, 4.0, 2.0, 3.0, 2.0, 3.0, 3.0, 4.0, 5.0, 3.5, 3.0, 4.5, 4.0, 1.0]
-    return df
-end
-
-function createdummydatasettwo()
-    df = DataFrame()
-    df[!, :user] = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7]
-    df[!, :item] = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 4, 5, 6, 2, 4, 5]
-    df[!, :rating] = [2, 7, 3, 3, 8, 3, 3, 3, 1, 9, 3, 3, 2, 3, 10, 4, 3, 3, 4, 2, 4, 3, 4, 2, 3, 2, 3, 3, 4, 5, 9, 10, 7, 6, 8]
+    df[!, :user] = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6]
+    df[!, :item] = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 4]
+    df[!, :rating] = [2.5, 3.5, 3.0, 3.5, 2.5, 3.0, 3, 3.5, 1.5, 5, 3, 3.5, 2.5, 3.0, 3.5, 4.0, 3.5, 3.0, 4.0, 2.5, 4.5, 3.0, 4.0, 2.0, 3.0, 2.0, 3.0, 3.0, 4.0, 5.0]
     return df
 end
 ####
 
-@test true
+dataframe = createdummydataset()
+dataset = Persa.Dataset(dataframe)
+
+@testset "Resampling Tests" begin
+    @testset "Holdout" begin
+        margin = 0.5
+
+        ds_train, ds_test = EvaluationCF.HoldOut(dataset; margin = margin)[0]
+
+        @test length(ds_train) == length(dataset) * margin
+        @test length(ds_test) == length(dataset) * (1 - margin)
+    end
+
+    @testset "Holdout" begin
+        k = 10
+
+        folds = EvaluationCF.KFolds(dataset; k = k)
+
+        i = 0
+
+        for (ds_train, ds_test) in folds
+            i = i + 1
+            @test length(ds_train) == length(dataset) * (1 - (1 / k))
+            @test length(ds_test) == length(dataset) * (1 / k)
+        end
+
+        @test i == k
+    end
+end
+
+@testset "Metrics Tests" begin
+
+end
